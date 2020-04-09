6c3ae0483093c622c45494863cad31753f497845
Gerard Braad <me@gbraad.nl>
2020-04-09 11:31:42 +0000

    Return value and null-check

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 namespace commandprocessor {
 
     // Command processor
-    type CommandProcessor = (cmd: string) => void
+    type CommandProcessor = (cmd: string) => string
 
     // Array definition to allow string indexer
     interface CommandProcessors {
@@ -14,11 +14,12 @@ namespace commandprocessor {
         commandProcessors[prefix] = processor
     }
 
-    export function ProcessCommand(command: string) {
+    export function ProcessCommand(command: string) : string {
         let cmdPrefix = command.slice(0, 1)
         let cmdParams = command.slice(1, command.length)
 
         let processor = commandProcessors[cmdPrefix]
-        processor(cmdParams)
+        if(processor == null) return "ERR: Unknown prefix"
+        return processor(cmdParams)
     }
 }
